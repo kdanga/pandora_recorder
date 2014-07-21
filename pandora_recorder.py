a962e7e35277b3d2fc4cf6c30b221b318fd1f980
kdanga <kdanga69@gmail.com>
2014-07-20 20:12:23 -0400

    Push of what I started so far.

---
+++ pandora_recorder.py
@@ -0,0 +1,19 @@
+import subprocess
+
+def sub():
+  try:
+    cmd = 'pianobar'
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+    output = p.stdout.readline()
+  except:
+    pass
+
+  while(output):
+    print output
+    output = p.stdout.readline()
+
+def main():
+  print 'yes'
+
+if __name__ == '__main__':
+  main()
